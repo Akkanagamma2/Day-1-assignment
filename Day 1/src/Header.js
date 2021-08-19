28d04f0181e42e6635da99a09867b22d454787be
Akkanagamma2 <83946029+Akkanagamma2@users.noreply.github.com>
2021-08-19 22:56:01 +0530

    Add files via upload

---
+++ Day 1/src/Header.js
@@ -0,0 +1,15 @@
+
+
+function Header(props){
+    return(
+           <div>
+               <h3>this is the line from the header component</h3>
+               <h3>Written By {props.name}</h3>
+               
+           </div>
+           
+           
+           
+    )
+}
+export default Header;
---
+++ Day 1/src/Nav.js
@@ -0,0 +1,12 @@
+
+
+
+function Nav(){
+    return(
+        <div>
+            <p>This is the line from the Nav component where app component is the parent of Nav</p>
+        </div>
+    )
+}
+
+export default Nav;
